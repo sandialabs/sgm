4caa382f8c9dd2bfcabbf2b2808430212d60cfc3
Paul Stallings <prstall@sandia.gov>
2018-05-15 08:54:38 -0600

    second try to get things right

---
+++ Source/Hermite.cpp
@@ -0,0 +1,83 @@
+#include "SGMDataClasses.h"
+#include "SGMMathematics.h"
+#include "SGMEnums.h"
+#include "SGMEntityClasses.h"
+#include "EntityClasses.h"
+#include "Curve.h"
+#include "Faceter.h"
+#include <algorithm>
+
+namespace SGMInternal
+{
+
+hermite::hermite(SGM::Result                      &rResult,
+                 std::vector<SGM::Point3D>  const &aPoints,
+                 std::vector<SGM::Vector3D> const &aTangents,
+                 std::vector<double>        const &aParams):  
+    curve(rResult,SGM::HermiteCurveType),m_aPoints(aPoints),m_aTangents(aTangents),m_aParams(aParams)
+    {
+    m_Domain.m_dMin=m_aParams.front();
+    m_Domain.m_dMax=m_aParams.back();
+    if(SGM::NearEqual(m_aPoints.front(),m_aPoints.back(),SGM_MIN_TOL))
+        {
+        m_aPoints.back()=m_aPoints.front();
+        m_bClosed=true;
+        }
+    }
+
+std::vector<SGM::Point3D> const &hermite::GetSeedPoints() const
+    {
+    if(m_aSeedPoints.empty())
+        {
+        FacetOptions Options;
+        Options.m_dFreeEdgeAngleTol=0.52359877559829887307710723054658; // 30 degrees.
+        FacetCurve(this,m_Domain,Options,m_aSeedPoints,&m_aSeedParams);
+        }
+    return m_aSeedPoints;
+    }
+
+std::vector<double> const &hermite::GetSeedParams() const
+    {
+    if(m_aSeedPoints.empty())
+        {
+        FacetOptions Options;
+        Options.m_dFreeEdgeAngleTol=0.52359877559829887307710723054658; // 30 degrees.
+        FacetCurve(this,m_Domain,Options,m_aSeedPoints,&m_aSeedParams);
+        }
+    return m_aSeedParams;
+    }
+
+size_t hermite::FindSpan(double t) const
+    {
+    size_t nSpan;
+    if(t<=m_aParams.front())
+        {
+        nSpan=0;
+        }
+    else if(m_aParams.back()<=t)
+        {
+        nSpan=m_aParams.size()-2;
+        }
+    else
+        {
+        nSpan=(size_t)(std::upper_bound(m_aParams.begin(),m_aParams.end(),t)-m_aParams.begin());
+        }
+    return nSpan;
+    }
+
+void hermite::Concatenate(hermite const *pEndHermite)
+    {
+    double dEndParam=m_aParams.back();
+    size_t nPoints=pEndHermite->m_aPoints.size();
+    size_t Index1;
+    for(Index1=1;Index1<nPoints;++Index1)
+        {
+        m_aParams.push_back(pEndHermite->m_aParams[Index1]+dEndParam);
+        m_aPoints.push_back(pEndHermite->m_aPoints[Index1]);
+        m_aTangents.push_back(pEndHermite->m_aTangents[Index1]);
+        }
+    m_aSeedParams.clear();
+    m_aSeedPoints.clear();
+    }
+
+} // End of SGMInternal namespace
---
+++ Source/NURBSurface.cpp
@@ -0,0 +1,60 @@
+#include "SGMDataClasses.h"
+#include "EntityClasses.h"
+#include "Surface.h"
+#include "Curve.h"
+#include "Faceter.h"
+
+namespace SGMInternal
+{
+NURBsurface::NURBsurface(SGM::Result                                   &rResult,
+                         std::vector<std::vector<SGM::Point4D> > const &aControlPoints,
+                         std::vector<double>                     const &aUKnots,
+                         std::vector<double>                     const &aVKnots):
+    surface(rResult,SGM::NUBSurfaceType),m_aaControlPoints(aControlPoints),m_aUKnots(aUKnots),m_aVKnots(aVKnots)
+    {
+    m_Domain.m_UDomain.m_dMin=aUKnots.front();
+    m_Domain.m_UDomain.m_dMax=aUKnots.back();
+    m_Domain.m_VDomain.m_dMin=aUKnots.front();
+    m_Domain.m_VDomain.m_dMax=aUKnots.back();
+
+    curve *pUCurve=UParamLine(rResult,m_Domain.m_UDomain.MidPoint(0.25));
+    curve *pVCurve=VParamLine(rResult,m_Domain.m_VDomain.MidPoint(0.25));
+    FacetOptions Options;
+    Options.m_dFreeEdgeAngleTol=0.52359877559829887307710723054658; // 30 degrees.
+    std::vector<double> aUParams,aVParams;
+    std::vector<SGM::Point3D> aUPoints,aVPoints;
+    FacetCurve(pUCurve,pUCurve->GetDomain(),Options,aUPoints,&aUParams);
+    FacetCurve(pVCurve,pVCurve->GetDomain(),Options,aVPoints,&aVParams);
+    rResult.GetThing()->DeleteEntity(pUCurve);
+    rResult.GetThing()->DeleteEntity(pVCurve);
+    size_t Index1,Index2;
+    m_nUParams=aUParams.size();
+    m_nVParams=aVParams.size();
+    size_t nParams=m_nUParams*m_nVParams;
+    m_aSeedParams.reserve(nParams);
+    m_aSeedPoints.reserve(nParams);
+    for(Index1=0;Index1<m_nUParams;++Index1)
+        {
+        double u=aUParams[Index1];
+        for(Index2=0;Index2<m_nVParams;++Index2)
+            {
+            double v=aVParams[Index2];
+            SGM::Point2D uv(u,v);
+            m_aSeedParams.push_back(uv);
+            SGM::Point3D Pos;
+            Evaluate(uv,&Pos);
+            m_aSeedPoints.push_back(Pos);
+            }
+        }
+    }
+
+std::vector<SGM::Point3D> const &NURBsurface::GetSeedPoints() const
+    {
+    return m_aSeedPoints;
+    }
+
+std::vector<SGM::Point2D> const &NURBsurface::GetSeedParams() const
+    {
+    return m_aSeedParams;
+    }
+}
