222b77b9e0b59cf6d460cfdc5965da4dd5ee539f
Byron Hanks <bwhanks@sandia.gov>
2018-05-30 16:41:45 -0600

    set warning level 4 and warnings as errors on Windows

--- SGMFunctions.cmake
+++ SGMFunctions.cmake
@@ -105,6 +105,22 @@ macro(sgm_project_setup)
     endif()
   endif()
 
+  if(MSVC)
+    # Force to always compile with W4
+    if(CMAKE_CXX_FLAGS MATCHES "/W[0-4]")
+      string(REGEX REPLACE "/W[0-4]" "/W4" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+      set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
+    else()
+      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W4")
+    endif()
+    # Force to always compile with WX
+    if(CMAKE_CXX_FLAGS MATCHES "/WX[-]")
+      string(REGEX REPLACE "/WX[-]" "/WX" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+    else()
+      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /WX")
+    endif()
+  endif(MSVC)
+
   include(GenerateExportHeader)
 
 endmacro()
