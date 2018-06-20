031e91bed3c9e48c96b2cb0c809619749236cbf6
Michael Plooster <mgploos@sandia.gov>
2018-06-20 10:57:04 -0600

    Add the ability to turn perspective on and off in the graphics view.

--- ModelViewer/ModelData.cpp
+++ ModelViewer/ModelData.cpp
@@ -40,7 +40,7 @@ ModelData::ModelData() :
   mwire_mode=false;
   mfacet_mode=false;
   muvspace_mode=false;
-  mperspective_mode=false;
+  mperspective_mode=true;
 }
 
 ModelData::~ModelData()
@@ -56,6 +56,8 @@ void ModelData::set_tree_widget(SGMTreeWidget *tree)
 void ModelData::set_graphics_widget(SGMGraphicsWidget *graphics)
 {
   dPtr->mGraphics = graphics;
+  dPtr->mGraphics->enable_perspective(mperspective_mode);
+  dPtr->mGraphics->set_render_faces(!mwire_mode);
 }
 
 bool ModelData::open_file(const QString &filename)
@@ -92,7 +94,7 @@ void ModelData::zoom()
 void ModelData::wire_mode()
 {
   mwire_mode = !mwire_mode;
-  rebuild_graphics();
+  dPtr->mGraphics->set_render_faces(!mwire_mode);
 }
 
 void ModelData::facet_mode()
@@ -109,8 +111,8 @@ void ModelData::uvspace_mode()
 
 void ModelData::perspective_mode()
 {
-  muvspace_mode = !muvspace_mode;
-  rebuild_graphics();
+  mperspective_mode = !mperspective_mode;
+  dPtr->mGraphics->enable_perspective(mperspective_mode);
 }
 
 bool ModelData::RunCPPTest(size_t nTest)
