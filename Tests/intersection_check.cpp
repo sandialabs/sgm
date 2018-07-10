728ad53babb7e379afa59a9beea623d69929d9c2
Byron Hanks <bwhanks@sandia.gov>
2018-07-10 08:14:03 -0600

    add some basic tests for line-plane and circle-plane intersections

--- Tests/intersection_check.cpp
+++ Tests/intersection_check.cpp
@@ -91,7 +91,7 @@ TEST(intersection_check, intersect_ellipse_and_plane)
     EXPECT_EQ(aTypes[0], SGM::IntersectionType::TangentType);
 
     // plane misses ellipse by slightly more than tolerance
-    PlaneOrigin = PlaneOrigin + (1.0e5)*PlaneNormal;
+    PlaneOrigin = PlaneOrigin + (1.0e-5)*PlaneNormal;
 
     aPoints.clear();
     aTypes.clear();
@@ -99,13 +99,118 @@ TEST(intersection_check, intersect_ellipse_and_plane)
 
     EXPECT_EQ(aPoints.size(), 0);
 
+    //// plane misses ellipse by slightly less than tolerance
+    //PlaneOrigin = PosOnEllipse + (1.0e-11)*PlaneNormal;
+
+    //aPoints.clear();
+    //aTypes.clear();
+    //SGM::IntersectCurveAndPlane(rResult, EllipseCurve2, PlaneOrigin, PlaneNormal, aPoints, aTypes, dTolerance);
+
+    //EXPECT_EQ(aPoints.size(), 1);
+    //EXPECT_EQ(aTypes.size(), 1);
+    //EXPECT_TRUE(SGM::NearEqual(PosOnEllipse, aPoints[0], dTolerance));
+    //EXPECT_EQ(aTypes[0], SGM::IntersectionType::TangentType);
+
     SGM::DeleteThing(pThing);
 }
 
-//size_t IntersectCurveAndPlane(SGM::Result                        &rResult,
-//                              curve                        const *pCurve,
-//                              SGM::Point3D                 const &PlaneOrigin,
-//                              SGM::UnitVector3D            const &PlaneNorm,
-//                              std::vector<SGM::Point3D>          &aPoints,
-//                              std::vector<SGM::IntersectionType> &aTypes,
-//                              double                              dTolerance);
+TEST(intersection_check, intersect_line_and_plane)
+{
+    SGMInternal::thing *pThing=SGM::CreateThing();
+    SGM::Result rResult(pThing);
+
+    SGM::Point3D Pos(-1,2,-3);
+    SGM::UnitVector3D Direction(1,2,3);
+    SGM::Curve LineID = SGM::CreateLine(rResult, Pos, Direction);
+
+    SGM::Point3D PlaneOrigin(0,0,0);
+    SGM::UnitVector3D PlaneNorm(-1,0,0);
+
+    double dTolerance = SGM_MIN_TOL;
+    std::vector<SGM::Point3D> aPoints;
+    std::vector<SGM::IntersectionType> aTypes;
+    SGM::IntersectCurveAndPlane(rResult, LineID, PlaneOrigin, PlaneNorm, aPoints, aTypes, dTolerance);
+
+    SGM::Point3D ExpectedPos(0,4,0);
+
+    EXPECT_EQ(aPoints.size(), 1);
+    EXPECT_EQ(aTypes.size(), 1);
+    EXPECT_TRUE(SGM::NearEqual(ExpectedPos, aPoints[0], dTolerance));
+    EXPECT_EQ(aTypes[0], SGM::IntersectionType::PointType);
+
+
+    PlaneOrigin = SGM::Point3D(-.5,3,-1.5);
+    PlaneNorm = Direction.Orthogonal();
+
+    aPoints.clear();
+    aTypes.clear();
+    SGM::IntersectCurveAndPlane(rResult, LineID, PlaneOrigin, PlaneNorm, aPoints, aTypes, dTolerance);
+
+    EXPECT_EQ(aPoints.size(), 2);
+    EXPECT_EQ(aTypes.size(), 2);
+    EXPECT_EQ(aTypes[0], SGM::IntersectionType::CoincidentType);
+    EXPECT_EQ(aTypes[1], SGM::IntersectionType::CoincidentType);
+
+    SGM::DeleteThing(pThing);
+}
+
+TEST(intersection_check, intersect_circle_and_plane)
+{
+    SGMInternal::thing *pThing=SGM::CreateThing();
+    SGM::Result rResult(pThing);
+
+    SGM::Point3D Center(7,6,-5);
+    SGM::UnitVector3D Normal(1,2,1);
+    double dRadius=5.5;
+    SGM::Curve CircleID = SGM::CreateCircle(rResult, Center, Normal, dRadius);
+
+    // coincident plane
+    SGM::Point3D PlaneOrigin = Center + 5*Normal.Orthogonal();
+    SGM::UnitVector3D PlaneNorm(-1,-2,-1);
+
+    double dTolerance = SGM_MIN_TOL;
+    std::vector<SGM::Point3D> aPoints;
+    std::vector<SGM::IntersectionType> aTypes;
+    SGM::IntersectCurveAndPlane(rResult, CircleID, PlaneOrigin, PlaneNorm, aPoints, aTypes, dTolerance);
+
+    EXPECT_EQ(aPoints.size(), 2);
+    EXPECT_EQ(aTypes.size(), 2);
+    EXPECT_EQ(aTypes[0], SGM::IntersectionType::CoincidentType);
+    EXPECT_EQ(aTypes[1], SGM::IntersectionType::CoincidentType);
+
+    // two intersection points
+    PlaneOrigin = Center + SGM::Vector3D(1,0,0);
+    PlaneNorm = SGM::UnitVector3D(0,1,0);
+
+    aPoints.clear();
+    aTypes.clear();
+    SGM::IntersectCurveAndPlane(rResult, CircleID, PlaneOrigin, PlaneNorm, aPoints, aTypes, dTolerance);
+
+    EXPECT_EQ(aPoints.size(), 2);
+    EXPECT_EQ(aTypes.size(), 2);
+    EXPECT_EQ(aTypes[0], SGM::IntersectionType::PointType);
+    EXPECT_EQ(aTypes[0], SGM::IntersectionType::PointType);
+    for (SGM::Point3D TestPos : aPoints)
+    {
+      EXPECT_NEAR(Center.Distance(TestPos), 5.5, dTolerance);
+    }
+
+    // tangent intersection
+    SGM::Point3D PosOnCircle;
+    SGM::UnitVector3D Tangent;
+    SGM::EvaluateCurve(rResult, CircleID, SGM_PI*0.7, &PosOnCircle, &Tangent);
+    PlaneOrigin = PosOnCircle + 3*Tangent;
+    PlaneNorm = Tangent*Normal + Normal; // 45 degrees to plane of circle
+
+    aPoints.clear();
+    aTypes.clear();
+    SGM::IntersectCurveAndPlane(rResult, CircleID, PlaneOrigin, PlaneNorm, aPoints, aTypes, dTolerance);
+
+    EXPECT_EQ(aPoints.size(), 1);
+    EXPECT_EQ(aTypes.size(), 1);
+    EXPECT_EQ(aTypes[0], SGM::IntersectionType::TangentType);
+    EXPECT_TRUE(SGM::NearEqual(PosOnCircle, aPoints[0], dTolerance));
+
+    SGM::DeleteThing(pThing);
+}
+
