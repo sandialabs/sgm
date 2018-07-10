fddbc4197c74e084a65785fd3cd856fb5543f858
Paul Stallings <prstall@sandia.gov>
2018-07-10 14:02:39 -0600

    One more try

---
+++ Source/Public_Headers/SGMAttribute.h
@@ -0,0 +1,32 @@
+#ifndef SGM_ATTRIBUTE_H
+#define SGM_ATTRIBUTE_H
+
+#include "SGMEntityClasses.h"
+#include "SGMVector.h"
+#include "SGMResult.h"
+
+#include <vector>
+
+#include "sgm_export.h"
+
+namespace SGM
+{
+
+SGM_EXPORT void FindAttributes(SGM::Result              &rResult,
+                               SGM::Entity        const &EntityID,
+                               std::set<SGM::Attribute> &sAttribute,
+                               bool                      bTopLevel=false);
+
+SGM_EXPORT SGM::EntityType GetAttributeType(SGM::Result          &rResult,
+                                            SGM::Attribute const &AttributeID);
+
+SGM_EXPORT std::string const &GetAttributeName(SGM::Result          &rResult,
+                                               SGM::Attribute const &AttributeID);
+
+SGM_EXPORT std::vector<int> const &GetIntegerAttributeData(SGM::Result          &rResult,
+                                                           SGM::Attribute const &AttributeID);
+
+
+} // End SGM namespace
+
+#endif // SGM_ATTRIBUTE_H
