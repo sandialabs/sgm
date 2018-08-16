2baadfdb8e42af6bb4539a23d6717d38c30ae91b
Kevin Copps <kdcopps@sandia.gov>
2018-08-16 14:28:26 -0600

    Refactor surface member functions with switch() into virtual

--- Source/Extrude.cpp
+++ Source/Extrude.cpp
@@ -36,10 +36,85 @@ void extrude::FindAllChildren(std::set<entity *, EntityCompare> &sChildren) cons
     sChildren.insert(m_pCurve);
     }
 
+void extrude::Evaluate(SGM::Point2D const &uv,
+                       SGM::Point3D       *Pos,
+                       SGM::Vector3D      *Du,
+                       SGM::Vector3D      *Dv,
+                       SGM::UnitVector3D  *Norm,
+                       SGM::Vector3D      *Duu,
+                       SGM::Vector3D      *Duv,
+                       SGM::Vector3D      *Dvv) const
+    {
+    SGM::Point3D   CurvePos;
+    SGM::Vector3D  DuCurve;
+    SGM::Vector3D *pDuCurve = &DuCurve;
+    SGM::Vector3D  DuuCurve;
+    SGM::Vector3D *pDuuCurve = &DuuCurve;
+
+    if (nullptr == Dv && nullptr == Duv && nullptr == Duu && nullptr == Norm)
+        pDuCurve = nullptr;
+    if (nullptr == Duu)
+        pDuuCurve = nullptr;
+
+    // Evaluate the defining curve.
+
+    m_pCurve->Evaluate(uv.m_u, &CurvePos, pDuCurve, pDuuCurve);
+
+    // Fill in the answers.
+
+    if(Pos)
+        {
+        *Pos=CurvePos+m_vAxis*uv.m_v;
+        }
+    if(Du)
+        {
+        *Du=*pDuCurve;
+        }
+    if(Dv)
+        {
+        *Dv=m_vAxis;
+        }
+    if(Norm)
+        {
+        *Norm=(*pDuCurve)*m_vAxis;
+        }
+    if(Duu)
+        {
+        *Duu=*pDuuCurve;
+        }
+    if(Duv)
+        {
+        *Duv=SGM::Vector3D(0,0,0);
+        }
+    if(Dvv)
+        {
+        *Dvv=SGM::Vector3D(0,0,0);
+        }
+    }
+
+SGM::Point2D extrude::Inverse(SGM::Point3D const &Pos,
+                              SGM::Point3D       *ClosePos,
+                              SGM::Point2D const *pGuess) const
+    {
+    SGM::Point2D uv;
+
+    uv.m_v=(Pos-m_Origin)%m_vAxis;
+    SGM::Point3D PlanePos=Pos-m_vAxis*uv.m_v;
+    if(pGuess)
+        {
+        uv.m_u=m_pCurve->Inverse(PlanePos,ClosePos,&(pGuess->m_u));
+        }
+    else
+        {
+        uv.m_u=m_pCurve->Inverse(PlanePos,ClosePos);
+        }
+    return uv;
+    }
+
+    
 void extrude::Transform(SGM::Transform3D const &Trans)
     {
     m_vAxis=Trans*m_vAxis;
-    curve *pCurve=m_pCurve;
     if(m_pCurve->GetEdges().empty() && m_pCurve->GetOwners().size()==1)
         {
         m_pCurve->Transform(Trans);
--- Source/Sphere.cpp
+++ Source/Sphere.cpp
@@ -37,6 +37,172 @@ sphere::sphere(SGM::Result             &rResult,
         }
     }
 
+void sphere::Evaluate(SGM::Point2D const &uv,
+                      SGM::Point3D       *Pos,
+                      SGM::Vector3D      *Du,
+                      SGM::Vector3D      *Dv,
+                      SGM::UnitVector3D  *Norm,
+                      SGM::Vector3D      *Duu,
+                      SGM::Vector3D      *Duv,
+                      SGM::Vector3D      *Dvv) const
+    {
+    double dCosU=cos(uv.m_u);
+    double dSinU=sin(uv.m_u);
+    double dCosV=cos(uv.m_v);
+    double dSinV=sin(uv.m_v);
+
+    if(Pos)
+        {
+        Pos->m_x=m_Center.m_x+((m_XAxis.m_x*dCosU+m_YAxis.m_x*dSinU)*dCosV+m_ZAxis.m_x*dSinV)*m_dRadius;
+        Pos->m_y=m_Center.m_y+((m_XAxis.m_y*dCosU+m_YAxis.m_y*dSinU)*dCosV+m_ZAxis.m_y*dSinV)*m_dRadius;
+        Pos->m_z=m_Center.m_z+((m_XAxis.m_z*dCosU+m_YAxis.m_z*dSinU)*dCosV+m_ZAxis.m_z*dSinV)*m_dRadius;
+        }
+    if(Du)
+        {
+        Du->m_x=(m_YAxis.m_x*dCosU-m_XAxis.m_x*dSinU)*dCosV*m_dRadius;
+        Du->m_y=(m_YAxis.m_y*dCosU-m_XAxis.m_y*dSinU)*dCosV*m_dRadius;
+        Du->m_z=(m_YAxis.m_z*dCosU-m_XAxis.m_z*dSinU)*dCosV*m_dRadius;
+        }
+    if(Dv)
+        {
+        Dv->m_x=(m_ZAxis.m_x*dCosV-(m_XAxis.m_x*dCosU+m_YAxis.m_x*dSinU)*dSinV)*m_dRadius;
+        Dv->m_y=(m_ZAxis.m_y*dCosV-(m_XAxis.m_y*dCosU+m_YAxis.m_y*dSinU)*dSinV)*m_dRadius;
+        Dv->m_z=(m_ZAxis.m_z*dCosV-(m_XAxis.m_z*dCosU+m_YAxis.m_z*dSinU)*dSinV)*m_dRadius;
+        }
+    if(Norm)
+        {
+        Norm->m_x=(m_XAxis.m_x*dCosU+m_YAxis.m_x*dSinU)*dCosV+m_ZAxis.m_x*dSinV;
+        Norm->m_y=(m_XAxis.m_y*dCosU+m_YAxis.m_y*dSinU)*dCosV+m_ZAxis.m_y*dSinV;
+        Norm->m_z=(m_XAxis.m_z*dCosU+m_YAxis.m_z*dSinU)*dCosV+m_ZAxis.m_z*dSinV;
+        }
+    if(Duu)
+        {
+        Duu->m_x=(-m_YAxis.m_x*dSinU-m_XAxis.m_x*dCosU)*dCosV*m_dRadius;
+        Duu->m_y=(-m_YAxis.m_y*dSinU-m_XAxis.m_y*dCosU)*dCosV*m_dRadius;
+        Duu->m_z=(-m_YAxis.m_z*dSinU-m_XAxis.m_z*dCosU)*dCosV*m_dRadius;
+        }
+    if(Duv)
+        {
+        Duv->m_x=(m_XAxis.m_x*dSinU-m_YAxis.m_x*dCosU)*dSinV*m_dRadius;
+        Duv->m_y=(m_XAxis.m_y*dSinU-m_YAxis.m_y*dCosU)*dSinV*m_dRadius;
+        Duv->m_z=(m_XAxis.m_z*dSinU-m_YAxis.m_z*dCosU)*dSinV*m_dRadius;
+        }
+    if(Dvv)
+        {
+        Dvv->m_x=(-m_ZAxis.m_x*dSinV-(m_XAxis.m_x*dCosU+m_YAxis.m_x*dSinU)*dCosV)*m_dRadius;
+        Dvv->m_y=(-m_ZAxis.m_y*dSinV-(m_XAxis.m_y*dCosU+m_YAxis.m_y*dSinU)*dCosV)*m_dRadius;
+        Dvv->m_z=(-m_ZAxis.m_z*dSinV-(m_XAxis.m_z*dCosU+m_YAxis.m_z*dSinU)*dCosV)*m_dRadius;
+        }
+    }
+
+SGM::Point2D sphere::Inverse(SGM::Point3D const &Pos,
+                              SGM::Point3D       *ClosePos,
+                              SGM::Point2D const *pGuess) const
+    {
+    double x=Pos.m_x-m_Center.m_x;
+    double y=Pos.m_y-m_Center.m_y;
+    double z=Pos.m_z-m_Center.m_z;
+
+    double dUx=x*m_XAxis.m_x+y*m_XAxis.m_y+z*m_XAxis.m_z;
+    double dUy=x*m_YAxis.m_x+y*m_YAxis.m_y+z*m_YAxis.m_z;
+
+    double dU=SGM::SAFEatan2(dUy,dUx);
+    double dV=0.0;
+    SGM::Vector3D VSpoke=(Pos-m_ZAxis*((Pos-m_Center)%m_ZAxis))-m_Center;
+    if(VSpoke.Magnitude()<SGM_ZERO)
+        {
+        if((Pos-m_Center)%m_ZAxis<0)
+            {
+            // South Pole
+            dV=-SGM_HALF_PI;
+            }
+        else
+            {
+            // North Pole
+            dV=SGM_HALF_PI;
+            }
+        }
+    else
+        {
+        SGM::UnitVector3D Spoke=VSpoke;
+        double dVx=x*Spoke.m_x+y*Spoke.m_y+z*Spoke.m_z;
+        double dVy=x*m_ZAxis.m_x+y*m_ZAxis.m_y+z*m_ZAxis.m_z;
+        dV=SGM::SAFEatan2(dVy,dVx);
+        }
+
+    if(dU<m_Domain.m_UDomain.m_dMin)
+        {
+        dU+=SGM_TWO_PI;
+        }
+    if(pGuess)
+        {
+        // Check for points on the north-south pole axis, at the
+        // center, and on the seam.
+
+        if(SGM::NearEqual(Pos,m_Center,SGM_MIN_TOL))
+            {
+            dU=pGuess->m_u;
+            dV=pGuess->m_v;
+            }
+        else if(m_Domain.m_UDomain.InInterval(dU,SGM_ZERO)==false)
+            {
+            if( SGM::NearEqual(pGuess->m_u,m_Domain.m_UDomain.m_dMax,SGM_MIN_TOL,false) &&
+                SGM::NearEqual(dU,m_Domain.m_UDomain.m_dMin,SGM_MIN_TOL,false))
+                {
+                dU=m_Domain.m_UDomain.m_dMax;
+                }
+            else if( SGM::NearEqual(pGuess->m_u,m_Domain.m_UDomain.m_dMin,SGM_MIN_TOL,false) &&
+                     SGM::NearEqual(dU,m_Domain.m_UDomain.m_dMax,SGM_MIN_TOL,false))
+                {
+                dU=m_Domain.m_UDomain.m_dMin;
+                }
+            }
+        else if(SGM::NearEqual(fabs(SGM::UnitVector3D(Pos-m_Center)%m_ZAxis),1.0,SGM_MIN_TOL,false))
+            {
+            dU=pGuess->m_u;
+            }
+        }
+
+    if(ClosePos)
+        {
+        SGM::UnitVector3D Ray=Pos-m_Center;
+        *ClosePos=m_Center+Ray*m_dRadius;
+        }
+    return {dU, dV};
+    }
+
+bool sphere::IsSame(surface const *pOther,double dTolerance) const
+    {
+    if (pOther->GetType() != m_SurfaceType)
+        return false;
+    bool bAnswer=true;
+    sphere const *pSphere1=(sphere const *)this;
+    sphere const *pSphere2=(sphere const *)pOther;
+    if(SGM::NearEqual(pSphere1->m_dRadius,pSphere2->m_dRadius,dTolerance,false)==false)
+        {
+        bAnswer=false;
+        }
+    else if(SGM::NearEqual(pSphere1->m_Center,pSphere2->m_Center,dTolerance)==false)
+        {
+        bAnswer=false;
+        }
+    return bAnswer;
+    }
+
+void sphere::PrincipleCurvature(SGM::Point2D const &uv,
+                                SGM::UnitVector3D  &Vec1,
+                                SGM::UnitVector3D  &Vec2,
+                                double             &k1,
+                                double             &k2) const
+    {
+    k1=1.0/m_dRadius;
+    k2=1.0/m_dRadius;
+    SGM::Vector3D dU,dV;
+    Evaluate(uv,nullptr,&dU,&dV);
+    Vec1=dU;
+    Vec2=dV;
+    }
+
 void sphere::Transform(SGM::Transform3D const &Trans)
     {
     m_Center=Trans*m_Center;
