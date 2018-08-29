23106e645e6a949e540e612d64148d962431dbc7
Byron Hanks <bwhanks@sandia.gov>
2018-08-29 09:37:09 -0600

    fix build warnings/errors on Windows

--- Source/Line.cpp
+++ Source/Line.cpp
@@ -98,7 +98,7 @@ void line::Transform(SGM::Transform3D const &Trans)
     m_Axis=Trans*m_Axis;
     }
 
-double line::FindLength(SGM::Interval1D const &Domain,double ) const
+double line::FindLength(SGM::Interval1D const &Domain,double ) const //dTolerance) const
     {
     return Domain.Length();
     }
